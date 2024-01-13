614a2e7ee28c95b5c97bc62e22c87c486ce4b080
astranero <esa.taheresa@gmail.com>
2024-01-13 21:12:09 +0200

    Puhelinluettelo v1

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,34 +1,100 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Number = ({name, number}) => {
+  return (
+    <p> {name} {number} </p>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <h2>  {text} </h2>
+  )
+}
+
+const Value = ({value, onChange, text}) => {
+  return (
+    <div>
+      {text}: <input value={value} onChange={onChange} /> 
+    </div>
+  )
+}
+
+const Persons = ({filteredPersons}) => {
+  return  filteredPersons.map(
+    (person) => <Number key={person.name} name={person.name} number={person.number}/>
+     )
+}
+
+const PersonForm = ({onSubmit, newName, newNumber, handleNameChange, handleNumberChange}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <Value value={newName} onChange={handleNameChange} text={"value"} />
+      <Value value={newNumber} onChange={handleNumberChange} text={"phone"} />
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const App = () => {
+  
+  const [persons, setPersons] = useState([
+      { name: 'Arto Hellas', number: '040-123456' },
+      { name: 'Ada Lovelace', number: '39-44-5323523' },
+      { name: 'Dan Abramov', number: '12-43-234345' },
+      { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
+  
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setNewFilter] = useState()
+  const [filteredPersons, setfilteredPersons] = useState([...persons])
+
+  const addName = (event) => {
+    event.preventDefault()
+    const currentPersons = [...persons]
+    const nameExists = currentPersons.some( person => {return person.name == newName})
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      currentPersons.push({name: newName, number: newNumber})
+      const filteredPersons = currentPersons.filter((person) => person.name.includes(filter))
+      setfilteredPersons(filteredPersons) 
+      setPersons(currentPersons)
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value
+    setNewFilter(filterValue)
+
+    const currentPersons = [...persons]
+    const filteredPersons = currentPersons.filter((person) => person.name.includes(filter))
+    setfilteredPersons(filteredPersons) 
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header text={"Phonebook"} />
+      <Value value={filter} onChange={handleFilterChange} text={"filter shown with"}/>
+      <Header text={"Add a new"} />
+      <PersonForm onSubmit={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <Header text={"Numbers"} />
+      <Persons filteredPersons={filteredPersons} />
+      ...
+    </div>
   )
 }
 
