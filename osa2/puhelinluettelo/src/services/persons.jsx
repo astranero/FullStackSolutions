28e16362b69af98c12e681f51590ba764a535b9b
astranero <esa.taheresa@gmail.com>
2025-02-05 17:03:11 +0200

    change url back to /api/persons

--- osa2/puhelinluettelo/src/services/persons.jsx
+++ osa2/puhelinluettelo/src/services/persons.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 const port = process.env.BACKEND_PORT || 3001
-const INITIAL_URL = `http://localhost:${port}/api/persons`
+const INITIAL_URL = `/api/persons`
 
 const getAll = () => {
     console.log("effect")
